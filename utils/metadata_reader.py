71c1e3ee8452822f705e1bb054d04c28dc7ea600
ingridpierre <ingridvpierre@gmail.com>
2025-02-10 01:56:35 +0000

    Assistant checkpoint
    Assistant generated file changes:
    - utils/metadata_reader.py: Add debug output and timeout
    
    ---
    
    User prompt:
    
    that new metadata_ready.py file doesn't appear to do anything, I run it and it just hangs.

--- utils/metadata_reader.py
+++ utils/metadata_reader.py
@@ -1,33 +1,40 @@
 
 import os
 import sys
-import xml.etree.ElementTree as ET
+import select
+import time
 
 def read_metadata():
     pipe_path = '/tmp/shairport-sync-metadata'
     
+    print(f"Checking for pipe at {pipe_path}...")
     if not os.path.exists(pipe_path):
-        print(f"Pipe not found at {pipe_path}")
+        print(f"Error: Pipe not found at {pipe_path}")
         return
-        
+    
+    print("Found metadata pipe!")
+    print("Waiting for AirPlay music to play (press Ctrl+C to exit)...")
+    
     with open(pipe_path, 'rb') as pipe:
         while True:
-            try:
-                data = pipe.read(4096).decode('utf-8', errors='ignore')
-                if data:
-                    # Parse XML-style data
-                    try:
+            # Use select to wait for data with timeout
+            ready, _, _ = select.select([pipe], [], [], 5.0)
+            if ready:
+                try:
+                    data = pipe.read(4096).decode('utf-8', errors='ignore')
+                    if data:
                         for item in data.split('</item>'):
-                            if 'asar' in item:  # Artist
-                                print("Artist:", item.split('<data>')[1].split('</data>')[0])
-                            elif 'minm' in item:  # Title
+                            if 'asar' in item:
+                                print("\nArtist:", item.split('<data>')[1].split('</data>')[0])
+                            elif 'minm' in item:
                                 print("Title:", item.split('<data>')[1].split('</data>')[0])
-                            elif 'asal' in item:  # Album
+                            elif 'asal' in item:
                                 print("Album:", item.split('<data>')[1].split('</data>')[0])
-                    except:
-                        pass
-            except KeyboardInterrupt:
-                break
+                except KeyboardInterrupt:
+                    print("\nExiting...")
+                    break
+            else:
+                print(".", end="", flush=True)  # Show waiting indicator
 
 if __name__ == '__main__':
     read_metadata()
